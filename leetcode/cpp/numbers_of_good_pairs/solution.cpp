faa9c321b627650a21fe3d684f91d1f37f8713a6
Popescu Ovidiu Ștefan <stefanpopescu923@gmail.com>
2020-10-16 00:46:52 +0300

    O(N) time complexity approach
    O(N) time complexity
    O(N) auxiliary space (worst case)

--- leetcode/cpp/numbers_of_good_pairs/solution.cpp
+++ leetcode/cpp/numbers_of_good_pairs/solution.cpp
@@ -2,11 +2,13 @@ class Solution {
 public:
     int numIdenticalPairs(vector<int>& nums) {
         int count = 0;
+        unordered_map<int, int> map;
         
         for (int i = 0; i < nums.size(); i++)
-            for (int j = i + 1; j < nums.size(); j++)
-                if (nums[i] == nums[j])
-                    count++;
+            map[nums[i]]++;
+        
+        for (auto p : map)
+            count += p.second * (p.second - 1) / 2;
         
         return count;
     }
