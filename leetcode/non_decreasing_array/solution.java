42a5f3bfafcb98a916546b414e9d740507981e91
Popescu Ovidiu Ștefan <stefanpopescu923@gmail.com>
2020-11-10 16:19:29 +0200

    O(N) time complexity approach

---
+++ leetcode/non_decreasing_array/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean checkPossibility(int[] nums) {
+        int pos = -1, n = nums.length - 1;
+        
+        for (int i = 1; i <= n; i++) {
+            if (nums[i - 1] > nums[i]) {
+                if (pos != - 1) {
+                    return false;
+                }
+                pos = i;
+            }
+        }
+        
+        return (pos == -1) ||
+            (pos == 1 || pos == n) ||
+            (nums[pos - 1] <= nums[pos + 1] || nums[pos - 2] <= nums[pos]);
+    }
+}
