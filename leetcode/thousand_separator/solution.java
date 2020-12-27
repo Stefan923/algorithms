9e09c87dc90f428d73956f6f711823901481c949
Popescu Ovidiu Ștefan <stefanpopescu923@gmail.com>
2020-12-28 00:22:28 +0200

    Improved solution - 0ms

--- leetcode/thousand_separator/solution.java
+++ leetcode/thousand_separator/solution.java
@@ -1,23 +1,13 @@
 class Solution {
     public String thousandSeparator(int n) {
-        if (n == 0) {
-            return "0";
+        StringBuilder stringBuilder = new StringBuilder(String.valueOf(n));
+        int size = stringBuilder.length() - 3;
+    
+        while (size > 0) {
+            stringBuilder.insert(size, ".");
+            size -= 3;
         }
         
-        int needsDot = 0;
-        String output = "";
-        
-        while (n > 0) {
-            if (needsDot == 3) {
-                output = "." + output;
-                needsDot = 0;
-            }
-            
-            output = (n % 10) + output;
-            n /= 10;
-            ++needsDot;
-        }
-        
-        return output;
+        return stringBuilder.toString();
     }
 }
