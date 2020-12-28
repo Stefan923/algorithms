d4fd7d9e2f56df6d569bbf714b7a835274e17df2
Popescu Ovidiu Ștefan <stefanpopescu923@gmail.com>
2020-12-28 16:48:32 +0200

    Improved O(N) time complexity solution, less comparisons

--- leetcode/best_time_to_buy_and_sell_stock_ii/solution.java
+++ leetcode/best_time_to_buy_and_sell_stock_ii/solution.java
@@ -1,31 +1,10 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int profit = 0;
-        int idxV = -1; // valley index
-        int idxP = -1; // peak index
         
-        for (int i = 0; i < prices.length; ++i) {
-            if (i - 1 >= 0) {
-                if (i + 1 < prices.length) {
-                    if (prices[i - 1] > prices[i] && prices [i] < prices[i + 1]) {
-                        idxV = i;
-                    } else if (prices[i - 1] < prices[i] && prices [i] >= prices[i + 1]) {
-                        idxP = i;
-                    }
-                } else if (prices[i - 1] < prices[i]) {
-                    idxP = i;
-                }
-                if (prices[i - 1] == prices[i] && idxV == -1) {
-                    idxV = i;
-                }
-            } else if (i + 1 < prices.length && prices[i] < prices[i + 1]) {
-                idxV = i;
-            }
-            
-            if (idxV != -1 && idxP != -1) {
-                profit += prices[idxP] - prices[idxV];
-                idxV = idxP = -1;
-            }
+        for (int i = 0; i < prices.length - 1; ++i) {
+            if (prices[i] < prices[i + 1])
+                profit += prices[i + 1] - prices[i];
         }
         
         return profit;
