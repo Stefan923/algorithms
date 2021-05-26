9aac314c87e06d68c39e7eceeb6c5d8d3b471ebc
Popescu Ovidiu Ștefan <stefanpopescu923@gmail.com>
2021-05-26 17:31:39 +0300

    O(N) time complexity approach

---
+++ leetcode/symmetric_tree/solution.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    
+    public boolean isSymmetric(TreeNode root) {
+        return isSymmetricRecursive(root.left, root.right);
+    }
+    
+    private boolean isSymmetricRecursive(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        
+        if (left != null && right != null && left.val == right.val) {
+            return isSymmetricRecursive(left.left, right.right) &&
+                isSymmetricRecursive(left.right, right.left);
+        }
+        
+        return false;
+    }
+    
+}
