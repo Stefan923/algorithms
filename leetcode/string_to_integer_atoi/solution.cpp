b4bdaa36692a40bd0e20004edc91e481d2d23696
Ovidiu Ștefan Popescu <stefanpopescu923@gmail.com>
2021-08-16 16:26:25 +0300

    O(N) time complexity approach

--- leetcode/string_to_integer_atoi/solution.cpp
+++ leetcode/string_to_integer_atoi/solution.cpp
@@ -1,1 +1,32 @@
-
+class Solution {
+    
+public:
+    int myAtoi(string s) {
+        long long number = 0;
+        int index = 0;
+        int sign = 1;
+        
+        while (s[index] == ' ') ++index;
+        
+        if (s[index] == '-') {
+            sign = -1;
+            ++index;
+        } else if (s[index] == '+') {
+            ++index;
+        }
+        
+        for (; s[index] >= '0' && s[index] <= '9'; ++index) {
+            number *= 10;
+            number += s[index] - '0';
+            
+            if (sign * number <= INT_MIN) {
+                return INT_MIN;
+            } else if (sign * number >= INT_MAX) {
+                return INT_MAX;
+            }
+        }
+        
+        return sign * number;
+    }
+    
+};
